590c504d9bd0f7433ffb1f5d44d6395bdc47025f
polytope <polytope@example.com>
2019-02-04 14:04:39 -0500

    Add webpack terser; compile in production mode.

--- webpack.production.js
+++ webpack.production.js
@@ -1,9 +1,13 @@
-var path = require('path');
+const path = require('path');
+const TerserPlugin = require('terser-webpack-plugin');
 
 module.exports = {
-  mode: 'development',
+  mode: 'production',
   entry: './src/initializeErlkonigLispConsole.js',
   devtool: 'inline-source-map',
+  optimization: {
+    minimizer: [new TerserPlugin()]
+  },
   output: {
     path: path.resolve(__dirname,  'public'),
     filename: 'erlkonig.js'
