74efad2ed0c5d2ab054a9de2beedd994fafe7ac0
polytope <polytope@example.com>
2019-02-04 14:33:56 -0500

    Add 'optimize-css-assets-webpack-plugin'.

--- webpack.production.js
+++ webpack.production.js
@@ -1,12 +1,16 @@
 const path = require('path');
+const OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin');
 const TerserPlugin = require('terser-webpack-plugin');
 
 module.exports = {
   mode: 'production',
   entry: './src/initializeErlkonigLispConsole.js',
-  devtool: 'inline-source-map',
+  devtool: 'source-map',
   optimization: {
-    minimizer: [new TerserPlugin()]
+    minimizer: [
+      new TerserPlugin(),
+      new OptimizeCssAssetsPlugin({})
+    ]
   },
   output: {
     path: path.resolve(__dirname,  'public'),
