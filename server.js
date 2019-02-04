fa41baa6e8d26208d7c167837dd5068924647fe7
polytope <polytope@example.com>
2019-02-04 15:06:56 -0500

    Remove hyphens from nonce; use 'let' and 'const'.

--- server.js
+++ server.js
@@ -1,23 +1,22 @@
 'use strict';
 
-var express = require('express');
-var fs      = require('fs');
-var http    = require('http');
-var path    = require('path');
+const express = require('express');
+const fs      = require('fs');
+const http    = require('http');
+const path    = require('path');
 
-var herokuHost   = 'erlkonig-lisp-console.herokuapp.com';
-var isProduction = !!(process.env.NODE_ENV === 'production');
-var host         = isProduction ? herokuHost : 'localhost';
+const herokuHost   = 'erlkonig-lisp-console.herokuapp.com';
+const isProduction = !!(process.env.NODE_ENV === 'production');
+const host         = isProduction ? herokuHost : 'localhost';
 
-var oneHour    = 60 * 60 * 1000;
-var httpConfig = { host: host };
-var port       = process.env.PORT || 5000;
-var utf8       = { encoding: 'utf8' };
-var signposts  = [8, 12, 16, 20];
+const oneHour    = 60 * 60 * 1000;
+const httpConfig = { host: host };
+const port       = process.env.PORT || 5000;
+const utf8       = { encoding: 'utf8' };
 
-var app              = express();
-var noncePlaceholder = /\$NONCE\$/g;
-var template         = fs.readFileSync('./templates/app', utf8);
+const app              = express();
+const noncePlaceholder = /\$NONCE\$/g;
+const template         = fs.readFileSync('./templates/app', utf8);
 
 if (!isProduction) {
   httpConfig.port = port;
@@ -42,7 +41,7 @@ function preventHerokuSleep() {
 }
 
 function _uuid(i, j) {
-  var nbr;
+  let nbr;
   if (i === 12) {
     nbr = 4;
   } else if (i == 16) {
@@ -54,18 +53,15 @@ function _uuid(i, j) {
 }
 
 function uuid() {
-  var id = '';
-  for (var i = 0; i < 32; i++) {
-    var random = Math.random() * 16 | 0;
-    if (signposts.indexOf(i) >= 0) {
-      id += '-';
-    }
+  let id = '';
+  for (let i = 0; i < 32; i++) {
+    const random = Math.random() * 16 | 0;
     id += _uuid(i, random);
   }
   return id;
 }
 
-var webpage = template.replace(noncePlaceholder, uuid());
+const webpage = template.replace(noncePlaceholder, uuid());
 
 app.use(express.static(path.join(__dirname, 'public'), { index: webpage }));
 
